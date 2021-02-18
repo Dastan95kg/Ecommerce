580051783bef81232c21b787c1942a32cfade184
Dastan95kg <dastan.maratov95@gmail.com>
2021-02-18 17:49:39 +0600

    [110] made basic layout on home page; got products from api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Home = () => (
-    <div>
-        <p>Home</p>
-    </div>
-)
+import ProductCard from '../components/cards/ProductCard';
+import { getProducts } from '../functions/product'
+
+const Home = () => {
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        loadAllProducts()
+    }, [])
+
+    const loadAllProducts = () => {
+        setLoading(true)
+        getProducts(3)
+            .then(res => {
+                setProducts(res.data)
+                setLoading(false)
+            })
+    }
+
+    return (
+        <>
+            <div className="jumbotron">
+                {loading ? <div>Loading...</div> : <div>All Products</div>}
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    {products.map(product => (
+                        <div
+                            key={product._id}
+                            className="col-md-4"
+                        >
+                            <ProductCard product={product} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
 
 export default Home;
